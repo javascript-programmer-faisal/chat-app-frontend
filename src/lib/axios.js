34f16da427783e3606b0fcae16aab65b87c99592
mern-coder-faisal <js.dev.faisal@gmail.com>
2025-06-01 01:36:29 +0600

    last commt

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-app-backend-q450.onrender.com/api"
+  baseURL: "https://chat-app-backend-q450.onrender.com/api",
   withCredentials: true,
 });
